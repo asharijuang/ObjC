41b61937a50633467f6bcff5ef6123cf77fad767
asharijuang <asharijuang@me.com>
2015-07-30 14:38:15 +0700

    Initial Commit

---
+++ Basic ObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Basic ObjC
+//
+//  Created by Ashari Juang on 7/30/15.
+//  Copyright (c) 2015 KodeJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Basic ObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Basic ObjC
+//
+//  Created by Ashari Juang on 7/30/15.
+//  Copyright (c) 2015 KodeJS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
